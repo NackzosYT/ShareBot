6a56e5bc1618c201f0abd907d5fb3daa87fe262f
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-09-18 22:11:54 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,25 @@
+const Discord = require('discord.js');
+const A7MD = new Discord.Client();
 
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+هلا وسهلا ياقلبي :hear_no_evil: 
+
+هذا سيرفر للالعاب والسوالف وغيرها :heart: 
+حتا نوزع جوائز وفي فعاليات يوميه
+لاتفوت كل هذا ادخل السيرفر
+:boxing_glove: 
+**
+احنا ما ندعو الا الراقين مثلك :flower_playing_cards: 
+[ ${member}  ] .......
+***{ https://discord.gg/VTemcwg }***`) 
+}).catch(console.error)
+})
+
+
+
+client.login(process.env.BOT_TOKEN);
