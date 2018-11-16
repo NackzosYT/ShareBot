3df359a39f8b57ff98dea8aac72e96b71f79a605
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-16 23:58:59 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,132 +1,56 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-
-console.log("BOT ONLINE");
+console.log("Welcome Again !");
  
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+  ***.#WeeeelcomeTo__Blood__.*** :heart_exclamation: 
+ **__خدماتنا في السيرفر___** :heart_eyes: 
+**
+- لدينا نضام دعوات اسطوري :wine_glass: 
+**
+.#5Invites,15k :beers: 
+.#10Invites,23k :beers: 
+.#15Invites,30k :beers: 
+.#20Invites,45k :beers: 
+**
+- ويوجد ليدنا متاجر :shopping_cart: 
+بيع توكنات ديسكورد
+وماينكرافت واشياء اخرى
 
+- وفعاليات وقيفاوايات يوميا :gift: 
+**
+:honey_pot:  ...واخيرا تنورنا ياعسل ${member}
+https://discord.gg/2kc5CPD
+`)
+}).catch(console.error)
+
+})
  
- 
-client.on('message', message => {
-  if (message.content.startsWith("!CS")) {
-   if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
-   message.guild.createRole({
-name: 'King',
-color: 'RANDOM',
-position: (1),
-permissions: 'ADMINISTRATOR'
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+  ***.#WeeeelcomeTo__Blood__.*** :heart_exclamation: 
+ **__خدماتنا في السيرفر___** :heart_eyes: 
+**
+- لدينا نضام دعوات اسطوري :wine_glass: 
+**
+.#5Invites,15k :beers: 
+.#10Invites,23k :beers: 
+.#15Invites,30k :beers: 
+.#20Invites,45k :beers: 
+**
+- ويوجد ليدنا متاجر :shopping_cart: 
+بيع توكنات ديسكورد
+وماينكرافت واشياء اخرى
+
+- وفعاليات وقيفاوايات يوميا :gift: 
+**
+:honey_pot:  ...واخيرا تنورنا ياعسل ${member}
+https://discord.gg/2kc5CPD
+`) 
+}).catch(console.error)
 })
-message.guild.createRole({
-  name: 'Prince',
-  color: 'RANDOM',
-  position: (2),
-  permissions: ['CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'BAN_MEMBERS', 'MANAGE_CHANNELS', 'ADD_REACTIONS', 'VIEW_AUDIT_LOG', 'VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES',
-      'SEND_TTS_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
-       'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES'],
-})
-message.guild.createRole({
-  name: 'Commander',
-  color: 'RANDOM',
-   position: (3),
-  permissions: ['CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'ADD_REACTIONS', 'VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
-  'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES']
-})
-message.guild.createRole({
-  name: 'Admin',
-  color: 'RANDOM',
-  postion: (4),
-  permissions: ['VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS',
-  'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME']
-})
-message.guild.createRole({
-  name: 'Vip',
-  color: 'RANDOM',
-  postion: (5),
-  permissions: ['VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS',
-  'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME']
-})
-message.guild.createRole({
-  name: 'Active',
-  color: '#030303',
-  postion: (6),
-  permissions: ['VIEW_CHANNEL', 'READ_MESSAGES', 'SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'CONNECT', 'SPEAK',
-  'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'ADD_REACTIONS']
-})
-message.channel.send('**Roles Was Succsesfluy Created**')
-.catch(console.error);
-  }
-});
-client.on('message', message => {
-  if (message.content.startsWith("!CS")) {
-  if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
-          if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`${message.author.username} You Dont Have ``MANAGE_CHANNELS`` **Premission**`);
-   message.guild.createChannel('rules', 'text')
-   message.guild.createChannel('welcome', 'text')
-   message.guild.createChannel('chat', 'text')
-   message.guild.createChannel('youtubers', 'text')
-   message.guild.createChannel('giveaway', 'text')
-   message.guild.createChannel('shop', 'text')
-   message.guild.createChannel('bot', 'text')
-   
-
-
-   
-
-message.channel.sendMessage('**Channels Was Succsesfluy Created**')
-}
-});
-
-client.on('message', message => {
-  if (message.content.startsWith("!CS")) {
-  if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
-          if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_CHANNELS`` **Premission**`);
-message.guild.createChannel('╔╣MUSIC╠╗', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('╔╣Public╠╗', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('╔╣MineCraft╠╗', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('╔╣Fortnite╠╗', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('╔╣Call Of Duty╠╗', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('╔╣League Of Legends╠╗', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('╔╣Battle Field╠╗', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('╔╣Cs : Go╠╗', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-message.guild.createChannel('Talk ◥', 'voice')
-.catch(console.error);
-  }
-});
-
-
-
-
-
-
-
- 
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-client.login(process.env.BOT_TOKEN); 
+client.login('NTAyODQwODA3MzM4OTM0Mjc2.DtCyXg.y5Oj1E8cTfZNuuYipZlNUI3r0VM');
