dfaa7bfe69ba253de4176fd4dc984a85896c642a
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-17 00:19:51 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,4 +53,44 @@ https://discord.gg/2kc5CPD
 `) 
 }).catch(console.error)
 })
+
+
+
+
+
+
+
+
+
+
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+  ***.#WeeeelcomeTo__Blood__.*** :heart_exclamation: 
+ **__خدماتنا في السيرفر___** :heart_eyes: 
+**
+- لدينا نضام دعوات اسطوري :wine_glass: 
+**
+.#5Invites,15k :beers: 
+.#10Invites,23k :beers: 
+.#15Invites,30k :beers: 
+.#20Invites,45k :beers: 
+**
+- ويوجد ليدنا متاجر :shopping_cart: 
+بيع توكنات ديسكورد
+وماينكرافت واشياء اخرى
+
+- وفعاليات وقيفاوايات يوميا :gift: 
+**
+:honey_pot:  ...واخيرا تنورنا ياعسل ${member}
+https://discord.gg/2kc5CPD
+`) 
+}).catch(console.error)
+})
+
+
+
+
+
+
 client.login('NTAyODQwODA3MzM4OTM0Mjc2.DtCz5Q.faKYb8yw2tARioQwV0q5pvqlGhk');
