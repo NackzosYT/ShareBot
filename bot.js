1878550424f7a5e18587c877d66b1b6ce2f0ac2f
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-12-15 15:49:47 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,40 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const A7MD = new Discord.Client();
 
-console.log("Welcome Again !");
+console.log("BOT ONLINE");
  
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**نضام انفايت خطير :leaves: 
+- 7 iNvite
+- 30k  :credit_card: 
+- 10 iNvite
+- 70k  :credit_card: 
+- 20 iNvite
+- 100k  :credit_card: 
+https://discord.gg/BWpZppJ
+ [ ${member}  ] :rose: ...
+**`) 
+}).catch(console.error)
+})
+
+
+
+
  client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
-  ***.#WeeeelcomeTo__Blood__.*** :heart_exclamation: 
- **__خدماتنا في السيرفر___** :heart_eyes: 
-**
-- لدينا نضام دعوات اسطوري :wine_glass: 
-**
-.#5Invites,15k :beers: 
-.#10Invites,23k :beers: 
-.#15Invites,30k :beers: 
-.#20Invites,45k :beers: 
-**
-- ويوجد ليدنا متاجر :shopping_cart: 
-بيع توكنات ديسكورد
-وماينكرافت واشياء اخرى
-
-- وفعاليات وقيفاوايات يوميا :gift: 
-**
-:honey_pot:  ...واخيرا تنورنا ياعسل ${member}
-https://discord.gg/2kc5CPD
-`)
+**نضام انفايت خطير :leaves: 
+- 7 iNvite
+- 30k  :credit_card: 
+- 10 iNvite
+- 70k  :credit_card: 
+- 20 iNvite
+- 100k  :credit_card: 
+https://discord.gg/BWpZppJ
+ [ ${member}  ] :rose: ...
+**`)
 }).catch(console.error)
 
 })
@@ -36,34 +46,12 @@ https://discord.gg/2kc5CPD
 
 
 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-  ***.#WeeeelcomeTo__Blood__.*** :heart_exclamation: 
- **__خدماتنا في السيرفر___** :heart_eyes: 
-**
-- لدينا نضام دعوات اسطوري :wine_glass: 
-**
-.#5Invites,15k :beers: 
-.#10Invites,23k :beers: 
-.#15Invites,30k :beers: 
-.#20Invites,45k :beers: 
-**
-- ويوجد ليدنا متاجر :shopping_cart: 
-بيع توكنات ديسكورد
-وماينكرافت واشياء اخرى
-
-- وفعاليات وقيفاوايات يوميا :gift: 
-**
-:honey_pot:  ...واخيرا تنورنا ياعسل ${member}
-https://discord.gg/2kc5CPD
-`) 
-}).catch(console.error)
-})
 
 
 
 
 
 
-client.login('NTAyODQwODA3MzM4OTM0Mjc2.DtCz5Q.faKYb8yw2tARioQwV0q5pvqlGhk');
+
+
+A7MD.login('NTAyOTE4NTg2NTI4OTU2NDE2.DvZ4Xg.ctWiApCtXbQAeZe_rqWsCv3MxK8');
